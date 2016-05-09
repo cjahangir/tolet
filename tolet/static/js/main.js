3e16aa2d6a602dfc327f9cb1deed3d0b6adb3223
cjahangir <jahangir@sdsdbd.com>
2016-05-09 23:26:06 +0600

    added repository tolet

---
+++ tolet/static/js/main.js
@@ -0,0 +1,14 @@
+function initMap() {
+        var myLatLng = {lat: 23.757955, lng: 90.363782};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+      }
